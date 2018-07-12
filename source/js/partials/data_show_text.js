4b6fd8442ab0eec51a21fe3f68f1457565b32832
Konstantin Elagin <kelagin@deep.social>
2018-07-12 20:06:15 +0300

    ch

---
+++ source/js/partials/data_show_text.js
@@ -0,0 +1,11 @@
+const elements = document.querySelectorAll('[data-show-text]');
+Array.prototype.forEach.call(elements, function(el, i){
+    let textElement = document.getElementById(el.getAttribute('data-show-text'));
+    console.log(textElement)
+    el.onclick = function (evt) {
+        el.remove();
+        textElement.style.display = '';
+        evt.preventDefault();
+        console.log(evt);
+    };
+});
--- source/js/project.js
+++ source/js/project.js
@@ -1,3 +1,4 @@
 import "../css/project.sass";
 import * as change_css_url from './partials/change_css_url.js';
 import * as mobile_menu from './partials/mobile_menu.js';
+import * as data_show_text from './partials/data_show_text.js';
