96baaab22679217940f7fa825c35a03d01b5ac64
Константин Елагин <konstantinelagin@MacBook-Air-Konstantin.local>
2018-07-22 02:42:47 +0300

    ch

--- source/js/partials/dynamics.js
+++ source/js/partials/dynamics.js
@@ -1,59 +1,95 @@
 window.dynamics = {
     draw: function (outsideData) {
 
-        // const mainData = outsideData.data;
+        const mainData = outsideData.data;
+        const startYear = outsideData.fromYear;
         const GRAPH_CONTAINER_ID = 'dynamicsContainer',
             GRAPH_CONTAINER_WRAPPER_ID = 'dynamicsContainerWrapper',
-            GRAPH_CONTAINER_INNER_ID = 'dynamicsContainerInner';
+            GRAPH_CONTAINER_INNER_ID = 'dynamicsContainerInner',
+            LEGEND_CONTSINER_ID = 'dynamicsLegend';
 
-        Highcharts.chart(GRAPH_CONTAINER_ID, {
-            title: {
-                text: null,
-            },
-            credits: {
-                enabled: false
-            },
-            yAxis: {
-                title: {
-                    text: null
-                }
-            },
-            tooltip: {
-                crosshairs: true
-            },
-            plotOptions: {
-                series: {
-                    label: {
-                        connectorAllowed: false
-                    },
-                    pointStart: 2010,
-                    marker: {
-                        fillColor: '#FFFFFF',
-                        lineWidth: 2,
-                        lineColor: null,
-                        symbol: 'circle'
-                    }
-                },
-                line: {
-                    enableMouseTracking: true
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            series: [{
-                name: 'Installation',
-                data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-                color: 'red'
-            }, {
-                name: 'Project Development',
-                data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-            }, {
-                name: 'Other',
-                data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-            }],
-        
+        let legendSections = '';
+
+        function collectLegendSections(callback) {
+
+            mainData.forEach(function (element, key) {
+                legendSections +=
+                    `<li class="dynamics__legend-item">
+                        <i style="background: ${element.color}" class="dynamics__legend-item-point"></i>
+                        <span class="dynamics__legend-text">${element.name}</span>
+                    </li>`;
+            });
+            callback();
+        };
+
+        collectLegendSections(function () {
+            let legendContainer = document.getElementById(LEGEND_CONTSINER_ID);
+            legendContainer.innerHTML = legendSections;
         });
 
+        let drawDynamics = function () {
+            Highcharts.chart(GRAPH_CONTAINER_ID, {
+                title: {
+                    text: null,
+                },
+                credits: {
+                    enabled: false
+                },
+                yAxis: {
+                    title: {
+                        text: null
+                    }
+                },
+                tooltip: {
+                    borderRadius: 8,
+                    backgroundColor: '#232c3d',
+                    borderWidth: 0,
+                    shadow: false,
+                    useHTML: true,
+                    style: {
+                        padding: 0,
+                        width: 420,
+                    },
+                    formatter: function () {
+                        return `
+                        <div class="dinamics-tooltip">
+                            <div class="dinamics-tooltip__date">
+                                ${this.point.x} г.
+                            </div>
+                            <div class="dinamics-tooltip__name">
+                                ${this.series.name}
+                            </div>
+                            <div class="dinamics-tooltip__number">
+                                ${this.point.y}%
+                            </div>
+                        </div> `;
+                    }
+                },
+                plotOptions: {
+                    series: {
+                        label: {
+                            connectorAllowed: false
+                        },
+                        pointStart: startYear,
+                        marker: {
+                            fillColor: '#FFFFFF',
+                            lineWidth: 3,
+                            lineColor: null,
+                            symbol: 'circle',
+                            radius: 6
+                        },
+                        lineWidth: 4
+                    },
+                    line: {
+                        enableMouseTracking: true
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                series: mainData
+            });
+        };
+        drawDynamics();
     }
 };
